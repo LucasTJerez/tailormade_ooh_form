4ff30cb886ff0e93dbe2bdaeac2e78f06f4da51f
Lucas Jerez <ltj2105@columbia.edu>
2020-08-29 14:30:40 +0200

    Updates

---
+++ precache-manifest.6de41e6b30a03d06928bcd9ae3563fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8ac8095cfbdecaa81b4c86d0c412cc",
+    "url": "/tailormade_ooh_form/index.html"
+  },
+  {
+    "revision": "e528c5978d9c62474e6a",
+    "url": "/tailormade_ooh_form/static/css/2.c24a8002.chunk.css"
+  },
+  {
+    "revision": "087937f3b90573984170",
+    "url": "/tailormade_ooh_form/static/css/main.ef26b1e4.chunk.css"
+  },
+  {
+    "revision": "e528c5978d9c62474e6a",
+    "url": "/tailormade_ooh_form/static/js/2.a7fceeae.chunk.js"
+  },
+  {
+    "revision": "c618162c7254fafac9264ad7b5080539",
+    "url": "/tailormade_ooh_form/static/js/2.a7fceeae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087937f3b90573984170",
+    "url": "/tailormade_ooh_form/static/js/main.051ec1c9.chunk.js"
+  },
+  {
+    "revision": "cdf1bf16663d6d80b361",
+    "url": "/tailormade_ooh_form/static/js/runtime-main.0896110e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/di-dashboard-ooh/precache-manifest.6d65604677e81d443a130c1d4f6ec17d.js"
+  "/tailormade_ooh_form/precache-manifest.6de41e6b30a03d06928bcd9ae3563fa5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/di-dashboard-ooh/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tailormade_ooh_form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
